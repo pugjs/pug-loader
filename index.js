2aadd173187a0d1b17415e8ff42a990cc52b6228
Tobias Koppers <tobias.koppers@googlemail.com>
2012-08-03 12:21:28 +0200

    fix to correct api

--- index.js
+++ index.js
@@ -2,19 +2,14 @@
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author Tobias Koppers @sokra
 */
-module.exports = function() {
+module.exports = function(source) {
 	this.cacheable && this.cacheable();
-	var options = this;
 	var jade = require("jade");
-	var results = [null];
 	var runtime = "var jade = require("+JSON.stringify(require("path").join(__dirname, "node_modules", "jade", "lib", "runtime"))+");\n\n";
-	Array.prototype.forEach.call(arguments, function(content, index) {
-		var tmplFunc = jade.compile(content, {
-			filename: options.filenames[index],
-			client: true,
-			compileDebug: options.debug || false
-		});
-		results[index+1] = runtime + "module.exports = " + tmplFunc.toString();
+	var tmplFunc = jade.compile(source, {
+		filename: this.filenames[0],
+		client: true,
+		compileDebug: this.debug || false
 	});
-	this.callback.apply(null, results);
+	return runtime + "module.exports = " + tmplFunc.toString();
 }

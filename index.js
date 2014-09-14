93e8c44856e5c1abb87ae7a18f158af107353798
zalad <coder.the@gmail.com>
2014-09-14 19:21:21 +0200

    make `extends` and `include` use aliases

--- index.js
+++ index.js
@@ -33,7 +33,14 @@ module.exports = function(source) {
 	MyParser.prototype.constructor = MyParser;
 
 	var missingFileMode = false;
+	var aliases = ((this.options || {}).resolve || {}).alias || {};
 	function getFileContent(context, request) {
+		var array = request.split('/');
+		var alias = array.shift();
+		if (aliases[alias]) {
+			context = aliases[alias];
+			request = array.join('/');
+		}
 		request = /^~/.test(request) ? request.substr(1) : "./" + request;
 		var baseRequest = request;
 		var filePath = filePaths[context + " " + request];

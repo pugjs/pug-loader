355a0f8d37fdb5fd9976691fdc5444cde129d599
Elvin G Marmol <egm0921@gmail.com>
2016-03-28 15:37:40 -0400

    Create Parser and JadeLoader class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,96 @@
-console.log('Start writing ES6, man!')
+import path from 'path'
+import loaderUtils from 'loader-utils'
+import jade from 'jade'
+import MyParser from './parser'
+
+let req = loaderUtils.getRemainingRequest(this).replace(/^!/, '')
+let query = loaderUtils.parseQuery(this.query)
+let missingFileMode = false
+let resolve, loadModule, loaderContext, callback
+let fileContents = {}
+let filePaths = {}
+
+export default class JadeLoader {
+  constructor (source) {
+    this.cacheable && this.cacheable()
+    loadModule = this.loadModule
+    resolve = this.resolve
+    loaderContext = this
+  }
+
+  getFileContent (context, request) {
+    request = loaderUtils.urlToRequest(request, query.root)
+    let baseRequest = request
+    let filePath = filePaths[`${context} ${request}`]
+    if (filePath) {
+      return filePath
+    }
+    let isSync = true
+    let self = this
+    resolve(context, `${request}.jade`, (err, _request) => {
+      if (err) {
+        resolve(context, request, (err2, _request) => {
+          if (err2) {
+            return callback(err2)
+          }
+
+          request = _request
+          next()
+        })
+        return
+      }
+
+      request = _request
+      next()
+      function next () {
+        loadModule(`-!${path.join(__dirname, 'stringify.loader.js')}!${request}`, function (err, source) {
+          if (err) {
+            return callback(err)
+          }
+
+          filePaths[context + ' ' + baseRequest] = request
+          fileContents[request] = JSON.parse(source)
+
+          if (!isSync) {
+            self.run()
+          } else {
+            isSync = false
+          }
+        })
+      }
+    })
+    if (isSync) {
+      isSync = false
+      missingFileMode = true
+      throw new Error('continue')
+    } else {
+      return filePaths[`${context} ${baseRequest}`]
+    }
+  }
+
+  run () {
+    let tmplFunc
+    try {
+      tmplFunc = jade.compile(this.source, {
+        parser: loadModule ? MyParser : undefined,
+        filename: req,
+        self: query.self,
+        globals: ['require'].concat(query.globals || []),
+        pretty: query.pretty,
+        locals: query.locals,
+        doctype: query.doctype || 'html',
+        compileDebug: loaderContext.debug || false
+      })
+    } catch (e) {
+      if (missingFileMode) {
+        // Ignore, it'll continue after async action
+        missingFileMode = false
+        return
+      }
+      throw e
+    }
+
+    loaderContext.callback(null, JSON.stringify(tmplFunc(query.locals)))
+  }
+
+}
---
+++ src/parser.js
@@ -0,0 +1,103 @@
+import JadeParser from 'jade/lib/parser'
+import filters from 'jade/lib/filters'
+import nodes from 'jade/lib/nodes'
+import utils from 'jade/lib/utils'
+
+export default class Parser extends JadeParser {
+  constructor (str, filename, options) {
+    super(str, filename, options)
+    this._mustBeInlined = false
+  }
+
+  parseMixin () {
+    this._mustBeInlined = true
+    return JadeParser.prototype.parseMixin.call(this)
+  }
+
+  parseBlock () {
+    this._mustBeInlined = true
+    return JadeParser.prototype.parseBlock.call(this)
+  }
+
+  parseCall () {
+    this._mustBeInlined = true
+    return JadeParser.prototype.parseCall.call(this)
+  }
+
+  parseExtends () {
+    if (!callback) {
+      callback = loaderContext.async()
+    }
+    if (!callback) {
+      return jade.Parser.prototype.parseExtends.call(this)
+    }
+
+    var request = this.expect('extends').val.trim()
+    var context = dirname(this.filename.split('!').pop())
+
+    var path = getFileContent(context, request)
+    var str = fileContents[path]
+    var parser = new this.constructor(str, path, this.options)
+
+    parser.blocks = this.blocks
+    parser.contexts = this.contexts
+    this.extending = parser
+
+    return new nodes.Literal('')
+  }
+
+  parseInclude () {
+    if (!callback) {
+      callback = loaderContext.async()
+    }
+    if (!callback) {
+      return jade.Parser.prototype.parseInclude.call(this)
+    }
+
+    var tok = this.expect('include')
+
+    var request = tok.val.trim()
+    var context = dirname(this.filename.split('!').pop())
+    var path = getFileContent(context, request)
+    var str = fileContents[path]
+
+    // has-filter
+    if (tok.filter) {
+      var hasFilterStr = str.replace(/\r/g, '')
+      var options = {filename: path}
+      var constantinople
+      if (tok.attrs) {
+        tok.attrs.attrs.forEach(function (attribute) {
+          options[attribute.name] = constantinople.toConstant(attribute.val)
+        })
+      }
+      hasFilterStr = filters(tok.filter, hasFilterStr, options)
+      return new nodes.Literal(hasFilterStr)
+    }
+
+    // non-jade
+    if (path.substr(-5) !== '.jade') {
+      var nonJadeStr = str.replace(/\r/g, '')
+      return new nodes.Literal(nonJadeStr)
+    }
+
+    var parser = new this.constructor(str, path, this.options)
+    parser.dependencies = this.dependencies
+
+    parser.blocks = utils.merge({}, this.blocks)
+    parser.included = true
+
+    parser.mixins = this.mixins
+
+    this.context(parser)
+    var ast = parser.parse()
+    this.context()
+    ast.filename = path
+
+    if (parser._mustBeInlined) {
+      this._mustBeInlined = true
+    }
+
+    return ast
+  }
+}
